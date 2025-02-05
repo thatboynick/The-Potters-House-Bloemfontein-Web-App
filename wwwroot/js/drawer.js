4d5eaf67de147c2d546b1f3193db597002a68628
thatboynick <you@example.com>
2025-02-05 15:44:59 +0200

    2025/02/05

---
+++ wwwroot/js/drawer.js
@@ -0,0 +1,14 @@
+﻿function toggleDrawer() {
+    const drawer = document.querySelector('.sidebar-drawer, .events-sidebar-drawer, .ministries-sidebar-drawer, .giving-sidebar-drawer');
+    const menuIcon = document.querySelector('.menu-icon i');
+
+    if (drawer.classList.contains('open')) {
+        drawer.classList.remove('open');
+        menuIcon.classList.remove('fa-times'); // Close icon
+        menuIcon.classList.add('fa-bars'); // Menu icon
+    } else {
+        drawer.classList.add('open');
+        menuIcon.classList.remove('fa-bars'); // Menu icon
+        menuIcon.classList.add('fa-times'); // Close icon
+    }
+}
